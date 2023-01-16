6c3218faa5effa139501887c8aec358faa4ad685
cassnova <daniel.rojascasanova@gmail.com>
2023-01-16 20:36:35 -0300

    added bd config

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,13 +2,10 @@ import mongoose from "mongoose";
 
 const connectBD = async () => {
   try {
-    const bd = await mongoose.connect(
-      "mongodb+srv://admin:admin@cluster0.hhbxfb9.mongodb.net/?retryWrites=true&w=majority",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
+    const bd = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     const url = `${bd.connection.host}:${bd.connection.port}`;
     console.log(`MongoDB conectado en: ${url}`);
   } catch (err) {
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 import express from "express";
+import dotenv from "dotenv";
 import connectBD from "./config/db.js";
 
 const app = express();
+dotenv.config();
 
 connectBD();
 
@@ -9,7 +11,7 @@ app.use("/", (req, res) => {
   res.send("Hola mundo");
 });
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Servidor funcionando en el puerto ${PORT}...`);
