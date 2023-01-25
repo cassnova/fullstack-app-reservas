c30ada9548e1a6deaa76f40cbf6a0c4ad9a3e6c5
cassnova <daniel.rojascasanova@gmail.com>
2023-01-24 21:43:56 -0300

    dynamic token

--- backend/routes/veterinarioRoutes.js
+++ backend/routes/veterinarioRoutes.js
@@ -1,9 +1,15 @@
 import express from "express";
 const router = express.Router();
-import { register, profile } from "../controllers/veterinarioController.js";
+import {
+  register,
+  profile,
+  confirmar,
+} from "../controllers/veterinarioController.js";
 
 router.post("/", register);
 
 router.get("/perfil", profile);
+
+router.get("/confirmar/:token", confirmar);
 
 export default router;
